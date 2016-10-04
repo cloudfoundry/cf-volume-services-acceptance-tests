e3d47f7c09145bad4b3029cd86023bf0b12228d6
Paul Warren <paul.warren@emc.com>
2016-10-04 16:19:40 -0700

    clean up bad pats runs.
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+orgs=$(cf orgs | grep PAT*)
+for org in ${orgs}; do
+
+	cf target -o ${org}
+	cf purge-service-instance -f $(cf services | grep PATs-pats-volume-instance* | awk '{print $1}')
+	cf target -o o
+  cf delete-org -f ${org}
+
+done
