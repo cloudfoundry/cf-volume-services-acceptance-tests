96660544d374e6cbe280367d89d76cd663edc875
Julian Hjortshoj <julian.hjortshoj@emc.com>
2019-07-29 10:05:33 -0700

    remove debug trace from pora-windows sample app and add error handling around New-SmbMapping command [#165428111](https://www.pivotaltracker.com/story/show/165428111)

--- assets/pora-windows/start.ps1
+++ assets/pora-windows/start.ps1
@@ -1,9 +1,3 @@
-echo "VCAP_SERVICES"
-echo "###############################################################################"
-echo $Env:VCAP_SERVICES
-echo "###############################################################################"
-echo $Env:VCAP_SERVICES > vcap-services.txt
-
 # look for a credentials block from a user provided service, a secure-credentials service, or a credhub service
 $base = echo $Env:VCAP_SERVICES | ./jq -c '.["""user-provided"""]'
 if ($base -eq "" -or $base -eq $null -or $base -eq "null") {
@@ -16,20 +10,21 @@ if ($base -eq "" -or $base -eq $null -or $base -eq "null") {
 if ($base -eq "" -or $base -eq $null -or $base -eq "null") {
   echo "Unable to find volume credentials...skipping mount"
 } else {
-  echo "FINAL-BASE:"
-  echo $base
   $smbshare=echo $base | ./jq -r '.[0].credentials.share'
   $smbuser=echo $base | ./jq -r '.[0].credentials.user'
   $smbpassword=echo $base | ./jq -r '.[0].credentials.password'
 
-  echo "USER: $smbuser"
-  echo "SHARE: $smbshare"
-  echo "password: $smbpassword"
-
+  # mount the remote share to Q:
+  $error.clear()
   New-SmbMapping -LocalPath 'Q:' -RemotePath $smbshare -UserName $smbuser -Password $smbpassword
+  if ($error) {
+    echo "Error: Unable to create SMB mount"
+    exit 1
+  }
 
   # inject the path for the new mount back into VCAP_SERVICES
   $Env:VCAP_SERVICES=echo $Env:VCAP_SERVICES | ./jq -r '. + {"smb":[{"volume_mounts":[{"container_dir":"""Q:""","device_type":"""shared""","mode":"""rw"""}]}]}'
 }
 
+# start the pora-windows application
 ./server.exe
